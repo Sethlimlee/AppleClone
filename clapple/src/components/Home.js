3443472ecadce4b8fe3139ade874903135229c55
SethLee <sethlimlee@gmail.com>
2018-06-06 11:54:46 -0600

    routes page set up.

---
+++ clapple/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  constructor(){
+    super()
+  }
+  render(){
+    return(
+      <div>
+        oh hey
+      </div>
+    )
+  }
+}
+export default Home
